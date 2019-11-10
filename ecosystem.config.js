3708fb0d242bb7211ad865a5738ca0d3689db72f
Seven Yu <dofyyu@gmail.com>
2019-11-10 20:03:19 +0800

    add ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [{
+    name: 'mFilter',
+    script: './bin/www',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    ignore_watch: ['views', 'public'],
+    env: {
+      PORT: 4000,
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      PORT: 4000,
+      NODE_ENV: 'production'
+    }
+  }],
+
+  /*
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+  */
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MIMEMI Filter' });
 });
+
+// create new
+router.get('/new', (req, res, next) => {
+  next(createError(401, 'Create New One'));
+})
+
+// suburl
+router.get('/:mid', (req, res, next) => {
+  const mid = req.params.mid
+  const query = req.query
+  next(createError(401, 'Subscribe MId: ' + mid));
+})
+
+// edit url
+router.get('/:mid/edit', (req, res, next) => {
+  const mid = req.params.mid
+  const query = req.query
+  next(createError(401, 'Edit MId: ' + mid));
+})
 
 module.exports = router;

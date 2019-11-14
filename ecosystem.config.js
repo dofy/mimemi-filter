c10b3173fb7845227a876d6e6eb54531dcbf4d60
Seven Yu <dofyyu@gmail.com>
2019-11-14 15:39:45 +0800

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,17 @@ module.exports = {
   apps : [{
     name: 'mFilter',
     script: './bin/www',
+    PORT: 4000,
     instances: 1,
     autorestart: true,
     max_memory_restart: '1G',
     ignore_watch: ['views', 'public'],
     env: {
       watch: true,
-      PORT: 4000,
       NODE_ENV: 'development'
     },
     env_production: {
       watch: false,
-      PORT: 4000,
       NODE_ENV: 'production'
     }
   }],
